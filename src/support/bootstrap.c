f0b660136041fc80e764b244833c52f4cc9303a4
Henrique Rieger <henriquerieger2001@gmail.com>
2024-05-28 23:35:05 -0300

    [add] comments to bootstrap.c

--- src/support/bootstrap.c
+++ src/support/bootstrap.c
@@ -26,10 +26,11 @@ void printReplicate(answer_t *answer, double treeWeight, FILE *fp) {
 
 // Perform bootstrap analysis by the giving method and number of replicates.
 void bootstrap(alignment_t *alignment, config_t *config) {
+  // Ensure characters begin set to 1
   resetCharacterWeights();
 
+  // Generate name of output file
   char filename[LABEL_SIZE];
-
   if (strlen(config->name) == 0)
     strcpy(filename, "intree");
   else {
@@ -37,8 +38,10 @@ void bootstrap(alignment_t *alignment, config_t *config) {
     strcat(filename, ".intree");
   }
 
+  // Open output file
   FILE *fp = fopen(filename, "w");
 
+  // Perform 1 analysis with equal weights
   answer_t *answer = config->searchMethod(alignment, config);
   double treeWeight = (double)1 / (getNumberOfTrees(answer));
   printReplicate(answer, treeWeight, fp);
@@ -46,6 +49,7 @@ void bootstrap(alignment_t *alignment, config_t *config) {
   printAnswer(answer, NULL);
   printf("\nGenerating %d bootstrap replicates...\n", config->bs_replicates);
 
+  // Generate n replicates with bootstrapped weights
   for (int i = 0; i < config->bs_replicates; i++) {
     printf("- Replicate %d\n", i + 1);
     destroyAnswer(answer);
@@ -55,6 +59,7 @@ void bootstrap(alignment_t *alignment, config_t *config) {
     printReplicate(answer, treeWeight, fp);
   }
 
+  // Close file and destroy leftover structures
   printf("Done. Check file '%s' for complete analysis.\n", filename);
   destroyAnswer(answer);
   fclose(fp);
