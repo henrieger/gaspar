7d8bf6279ca1997c75f048c2d5c2a84a28fe36e1
Henrique Rieger <henriquerieger2001@gmail.com>
2026-09-25 01:42:16 -0300

    [remove] internal functions from definitions of include files

--- include/gaspar/parsimony.h
+++ include/gaspar/parsimony.h
@@ -12,10 +12,4 @@ double fitchParsimony(tree_t *tree, config_t *config);
 // Calculate Wagner parsimony at a particular node of the tree
 double localParsimony(tree_t *tree, int32_t node);
 
-void initializeGlobalAuxSequences(uint32_t characters, uint32_t states);
-void destroyGlobalAuxSequences();
-
-void resetParsimonyCalls();
-unsigned long getParsimonyCalls();
-
 #endif // !__LIBGASPAR_PARSIMONY_H__
